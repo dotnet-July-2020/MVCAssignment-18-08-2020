f43903e9d95f8f2a3e9f8551712537986f15cda8
Geethika <geethika.duddupudi@zensar.com>
2020-08-19 21:58:29 +0530

    Creating product class using mvc

---
+++ AssignmentProduct/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using AssignmentProduct.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentProduct.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var product = GetProductsDeatils();
+            return View(product);
+            
+        }
+
+        
+
+        public IEnumerable<Product> GetProductsDeatils()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId=101 ,ProductName="Ac",Rate=45000},
+                new Product{ProductId=102 ,ProductName="Mobile",Rate=38000},
+                new Product{ProductId=103 ,ProductName="Bike",Rate=94000}
+            };
+        }
+        public ActionResult Deatils(int id)
+        {
+            var product = GetProductsDeatils().SingleOrDefault(c => c.ProductId == id);
+            if (product == null)
+                return HttpNotFound();
+            return View(product);
+        }
+    }
+}
